73d80c49331549bfcda658da833c9ebdd956034f
NomanSalhab <nomansalhab8516@gmail.com>
2022-10-10 14:48:31 +0300

    Applied Go Templates To Our HTML Pages.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -58,3 +58,28 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// Reservation Page Handler
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+}
+
+// Generals Page Handler Renders The Room Page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+}
+
+// Majors Page Handler Renders The Room Page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+}
+
+// Availability Page Handler Renders The Search Availability Page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{})
+}
+
+// Availability Page Handler Renders The Search Availability Page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.html", &models.TemplateData{})
+}
